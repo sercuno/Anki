5cf03d0993caea915b7c08940d145366851df385
Avi Rana <itzavi1718@gmail.com>
2023-03-12 23:30:52 +0530

    Minor changes

--- script/watch.js
+++ script/watch.js
@@ -8,7 +8,7 @@ fetch(`https://heavenlyaccomplishedcubase.avirana2.repl.co/url?id=${id}&ep=${ep}
 .then((data) => {
     console.log(data);
     document.getElementById("vid").src=data[0].servers[2].iframe;
-    document.getElementById("title").innerHTML=data[0].id;
+    document.getElementById("title").innerHTML=id;
 
 });
 
