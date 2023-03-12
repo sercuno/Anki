0e01faf6d759a8ea52ec7282b388b223a2722b6b
Avi Rana <itzavi1718@gmail.com>
2023-03-12 21:07:21 +0530

    Improved search

--- script/search.js
+++ script/search.js
@@ -1,11 +1,12 @@
 function search() {
-
+    document.getElementById("loading").style.display = "block";
+    
 var totalEp, title;
    
    fetch(`https://heavenlyaccomplishedcubase.avirana2.repl.co/info?id=${document.getElementById("search").innerHTML}`)
     .then((response) => response.json())
     .then((data) => {
-
+        document.getElementById("loading").style.display = "none";
         for(var i=0;i<=data.length;i++) {
             var ul = document.getElementById("list");
             var li = document.createElement("li");
